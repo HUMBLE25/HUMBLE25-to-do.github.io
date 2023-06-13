3a019da9312e626168ad8086a32c1307c3f08c81
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-13 15:37:48 +0900

    #4.4Getting Username 수강

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 
 const loginForm =document.querySelector("#login-form");
 const logInput = document.querySelector("#login-form input");
+const greeting =document.querySelector("#greeting")
 
-const link =document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden"
 
 const onLoginSubmit= (event) => {
     event.preventDefault(); 
+
     const userName = logInput.value;
-    console.log(`hello ${userName}`);
+
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    greeting.innerHTML = `hello ${userName}!!`;
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const handleLinkClick= (event)=>{
-    event.preventDefault();
-    console.dir(event);
-
-}
-
-loginForm.addEventListener("submit",onLoginSubmit)
-link.addEventListener("click",handleLinkClick)
-
-// 이벤트리스너의 두번째 인자로 들어가는 함수는 브라우저가 실행시켜준다.
-// 그리고 방금 일어난 이벤트 정보를 객체로 담아준다.
-
-
+loginForm.addEventListener("submit",onLoginSubmit);
