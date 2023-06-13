25a99326c688be472226cd5e2599db15c44d21d1
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-13 15:00:41 +0900

    #4.2 수강

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 
-
+const loginForm =document.querySelector("#login-form")
 const logInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-const onLoginBtnClick=()=>{
-    const userName = logInput.value
-    console.log(userName)
-    // !userName || userName.length >= 15 
-    //     ? alert("please write no more than 1 to 15 words") 
-    //     : alert("hello",userName);
+
+const onLoginSubmit= (info) => {
+   
+    // submit의 기본동작은 새로고침인데 이때문에 입력받은 정보를 받아올수 없다.
+    // 브라우저의 기본동작을 막아야하는데 이를 도와주는 매서드가 있다.
+    info.preventDefault(); 
+    // 브라우저의 기본동작을 막는 매서드이다.
+    const userName = logInput.value;
+    console.log(`hello ${userName}`);
 }
 
-const onPressEnter =(e)=>{
-    if(e.key === "Enter"){
-        onLoginBtnClick()
-    }
-}
+loginForm.addEventListener("submit",onLoginSubmit)
 
-loginButton.addEventListener("click",onLoginBtnClick)
-logInput.addEventListener("keypress",onPressEnter)
+
