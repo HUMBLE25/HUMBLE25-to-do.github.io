e804ba4dcf8a813647b0ea14d943b276a5877270
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-14 10:22:52 +0900

    #5.2 PadStart 수강

--- js/clock.js
+++ js/clock.js
@@ -2,14 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 const getClock=()=>{
     const date = new Date();
-    
-    // padStart()를 사용하기 위해 toString()으로 string type으로 만들어 주었다.
 
-    const hours = date.getHours().toString().padStart(2,"0");
+    // padStart()를 사용하기 위해으로 string type으로 만들어 주었다.
 
-    const minutes = date.getMinutes().toString().padStart(2,"0");
-
-    const seconds = date.getSeconds().toString().padStart(2,"0");
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
 
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 
