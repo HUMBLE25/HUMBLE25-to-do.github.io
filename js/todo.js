e1f28f5542ea823caf2bab8fdaea317071026eb8
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-14 22:20:37 +0900

    #7.5 Loading To Dos part Two 수강 및 보완

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,10 @@ const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoLists = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos"
-const toDos = [];
+
+const SavedToDs = JSON.parse(localStorage.getItem(TODOS_KEY));
+
+const toDos = SavedToDs ? SavedToDs : [];
 
 const saveToDo = () => {
     localStorage.setItem(TODOS_KEY,JSON.stringify(toDos))
@@ -50,17 +53,6 @@ const handleToDoSubmit = (event) => {
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
 
-const SavedToDs = localStorage.getItem(TODOS_KEY);
-
 if(SavedToDs){
-    const parsedToDos = JSON.parse(SavedToDs);
-
-    parsedToDos.forEach(paintTodo);
-    // 아래의 로직은 위의 한줄과 같은 기능을 하는 것들이다.
-
-    // for(let todo of parsedToDos){
-    //     paintTodo(todo);
-    // }
-
-    // parsedToDos.forEach(e => paintTodo(e))
+    SavedToDs.forEach(paintTodo);
 }
