b2fbc5e7b2a66383af3338690d688ef540fe1f64
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-14 15:37:44 +0900

    #7.1 Adding ToDos 수강

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,29 @@ const toDoForm =document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const paintTodo = (newTodo) => {
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+
+    // const button =document.createElement("button");
+
+    li.append(span);
+
+    // li.append(button);
+
+    span.innerText = newTodo;
+
+    toDoList.appendChild(li);
+}
+
 const handleToDoSubmit = (event) => {
     event.preventDefault();
 
     const newToDo = toDoInput.value;
-    
+
     toDoInput.value = "";
+    paintTodo(newToDo);
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit)
