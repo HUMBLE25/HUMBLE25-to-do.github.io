d3160f2d51f21017534ddde582811e68bec6913e
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-14 15:19:30 +0900

    #7.0 Setup

--- js/background.js
+++ js/background.js
@@ -6,11 +6,4 @@ const bgImg =document.createElement("img");
 
 bgImg.src = `img/${chosenImg}`;
 
-//body에서 가장 마지막에 추가하는 매서드 appendChild
-// mdn참고 append()와 appendChild의 차이점
-// Element.append() can append several nodes and strings, whereas Node.appendChild() can only append one node.
 document.body.appendChild(bgImg);
-// document.body.append(bgImg);
-
-// body에서 가장 먼저 추가하는 매서드 prepend 
-// document.body.prepend(bgImg);
