e42d0668c0c136fc3291787af691c877daa154f8
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-14 16:41:52 +0900

    #7.2Deleting To Dos 수강

--- js/todo.js
+++ js/todo.js
@@ -1,21 +1,33 @@
 const toDoForm =document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
-const toDoList = document.getElementById("todo-list");
+const toDoLists = document.getElementById("todo-list");
+
+const deleteToDo = (event)=>{
+    const li = event.target.parentElement;
+
+    li.remove();
+}
 
 const paintTodo = (newTodo) => {
+    console.dir(toDoLists.childNodes);
+
     const li = document.createElement("li");
 
     const span = document.createElement("span");
 
-    // const button =document.createElement("button");
-
-    li.append(span);
-
-    // li.append(button);
-
     span.innerText = newTodo;
 
-    toDoList.appendChild(li);
+    const button =document.createElement("button");
+
+    button.innerText = "❌";
+
+    button.addEventListener("click",deleteToDo);
+
+    li.appendChild(span);
+
+    li.appendChild(button);
+
+    toDoLists.appendChild(li);
 }
 
 const handleToDoSubmit = (event) => {
@@ -24,6 +36,7 @@ const handleToDoSubmit = (event) => {
     const newToDo = toDoInput.value;
 
     toDoInput.value = "";
+
     paintTodo(newToDo);
 }
 
