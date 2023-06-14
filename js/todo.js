cb466d8ee6af7afa691a14d803ba4bb2b35604d5
HUMBLE25 <wlsrua1007@gmail.com>
2023-06-14 17:04:48 +0900

    #7.3 Saving To Dos 수강

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,11 @@ const toDoForm =document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoLists = document.getElementById("todo-list");
 
+const toDos = [];
+
+const saveToDo = () => {
+    localStorage.setItem("toDos",JSON.stringify(toDos))
+}
 const deleteToDo = (event)=>{
     const li = event.target.parentElement;
 
@@ -9,8 +14,6 @@ const deleteToDo = (event)=>{
 }
 
 const paintTodo = (newTodo) => {
-    console.dir(toDoLists.childNodes);
-
     const li = document.createElement("li");
 
     const span = document.createElement("span");
@@ -37,7 +40,10 @@ const handleToDoSubmit = (event) => {
 
     toDoInput.value = "";
 
+    toDos.push(newToDo);
+
     paintTodo(newToDo);
+    saveToDo();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit)
